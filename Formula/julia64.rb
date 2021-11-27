283851dd3e3b8b59d694dfebf926f246e78c8342
Johannes Blaschke <jpblaschke@lbl.gov>
2021-11-27 11:34:13 +0100

    Debugging conflicts_with

--- Formula/julia64.rb
+++ Formula/julia64.rb
@@ -26,8 +26,7 @@ class Julia64 < Formula
   depends_on "utf8proc"
 
   # Ensure that we don't clobber the non 64-bit julia install
-  conflicts_with cask: "julia"
-  conflicts_with formula: "julia"
+  conflicts_with: "julia"
 
   uses_from_macos "perl" => :build
   uses_from_macos "zlib"
